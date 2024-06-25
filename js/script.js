972486d1e0982eadf767e597eaacf8d85fdfe301
gustavords <22622146+gustavords@users.noreply.github.com>
2024-06-25 02:14:07 -0400

    Create getComputerChoice(), getHumanChoice() and playRound() functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,97 @@
-console.log("im here!");
+// console.log("im here!");
+
+// Global Variables
+let humanScore = 0;
+let computerScore = 0;
+
+let getComputerChoice = () => {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    switch (computerChoice) {
+        case 1:
+            computerChoice = "rock";
+            break;
+        case 2:
+            computerChoice = "paper";
+            break;
+        case 3:
+            computerChoice = "scissors";
+            break;
+    }
+    return computerChoice;
+};
+
+// console.log(getComputerChoice());
+
+let getHumanChoice = () => {
+    let possibleChoices = ["rock", "paper", "scissors"];
+    let humanChoice = prompt("rock, paper or scissors?", "").toLowerCase().trim();
+    if (humanChoice == "" || !isNaN(+humanChoice)) {
+        console.log(`"${humanChoice}" CANNOT be empty or a number. Please type: rock, paper or scissors.`);
+        return getHumanChoice();
+    }
+    else if (!possibleChoices.includes(humanChoice)) {
+        console.log(`"${humanChoice}" is NOT rock paper or scissors. Please type: rock, paper or scissors`);
+        return getHumanChoice();
+    }
+    else {
+        return humanChoice;
+    }
+};
+
+// console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+
+    let roundTxt = (winner) => {
+        if (winner == "comp") {
+            console.log(`Human: ${humanChoice} | Computer: ${computerChoice}`);
+            console.log("Computer Wins Round");
+            console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+        }
+        else if (winner == "human") {
+            console.log(`Human: ${humanChoice} | Computer: ${computerChoice}`);
+            console.log("Human Wins Round");
+            console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+        } else {
+            console.log(`Human: ${humanChoice} | Computer: ${computerChoice}`);
+            console.log("Tie")
+            console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+        }
+
+    };
+
+    if (humanChoice == undefined) {
+        console.log("error")
+    }
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore++;
+        roundTxt("comp");
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+        roundTxt("human");
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+        roundTxt("human");
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore++;
+        roundTxt("comp");
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        roundTxt("human");
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore++;
+        roundTxt("comp");
+    }
+    else {
+        humanScore++;
+        computerScore++;
+        roundTxt();
+    }
+}
+
+//  playRound(getHumanChoice(), getComputerChoice());
