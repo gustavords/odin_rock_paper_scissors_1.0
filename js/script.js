2d7d95fa794acf5fefa1808550dbe6953a696d63
gustavords <22622146+gustavords@users.noreply.github.com>
2024-06-29 04:28:38 -0400

    Fix tie-game process in playFiveRounds() by making user play extra round

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@
 let humanScore = 0;
 let computerScore = 0;
 
-let getComputerChoice = () => {
+//referencing elements in index.html
+const buttonCollection = document.querySelectorAll(`.playerSelectionButton`);
+const roundPara = document.querySelector(`#round`);
+
+const roundNum = document.querySelector("h3.round-num");
+const roundSelection = document.querySelector("p.round-selection");
+const roundWinner = document.querySelector("p.round-winner");
+
+let currentPlayerChoice = ``;
+let rounds = 4;
+
+const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     switch (computerChoice) {
         case 1:
@@ -20,75 +31,8 @@ let getComputerChoice = () => {
     return computerChoice;
 };
 
-let getHumanChoice = () => {
-    let possibleChoices = ["rock", "paper", "scissors"];
-    let humanChoice = prompt("rock, paper or scissors?", "").toLowerCase().trim();
-    if (humanChoice == "" || !isNaN(+humanChoice)) {
-        console.log(`"${humanChoice}" CANNOT be empty or a number. Please type: rock, paper or scissors.`);
-        return getHumanChoice();
-    }
-    else if (!possibleChoices.includes(humanChoice)) {
-        console.log(`"${humanChoice}" is NOT rock paper or scissors. Please type: rock, paper or scissors`);
-        return getHumanChoice();
-    }
-    else {
-        return humanChoice;
-    }
-};
-
-//find buttons in page
-const buttonCollection = document.querySelectorAll(`.playerSelectionButton`);
-const roundPara = document.querySelector(`#round`);
-let currentPlayerChoice = ``;
-let rounds = 4;
-
-buttonCollection.forEach((button) => {
-    button.addEventListener(`click`, (e) => {
-        currentPlayerChoice = getHumanChoiceBtn(e).toLowerCase();
-        console.log(currentPlayerChoice);
-        playRound(currentPlayerChoice, getComputerChoice());
-        playFiveRounds();
-    });
-});
-
 function getHumanChoiceBtn(elem) {
     return elem.target.value;
-}
-
-const roundNum = document.querySelector("h3.round-num");
-const roundSelection = document.querySelector("p.round-selection");
-const roundWinner = document.querySelector("p.round-winner");
-
-
-function playFiveRounds() {
-    console.log(rounds);
-    if (rounds <= 0) {
-        (humanScore > computerScore) ? roundWinner.textContent = `HUMAN WINS GAME` : roundWinner.textContent = `COMP WINS GAME`;
-        //in case of a tie
-        if (rounds === 1 && computerScore === humanScore) {
-            roundWinner.textContent = `ITS a TIE! One more round!`;
-            rounds++;
-            if (humanScore > computerScore) {
-                roundWinner.textContent = `HUMAN WINS`
-            }
-            if (humanScore > computerScore) {
-                roundWinner.textContent = `COMP WINS`
-            };
-        }
-        else {
-
-            //resets game-rounds
-            rounds = 4;
-            humanScore = 0;
-            computerScore = 0;
-        }
-    }
-    else {
-        //issue with amount
-        //change layout for less confusing round timing
-        roundNum.textContent = `Round : ${5 - (rounds - 1)}`;
-        rounds--;
-    }
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -101,13 +45,13 @@ function playRound(humanChoice, computerChoice) {
             roundHist.insertBefore(clone, roundContainer.nextSibling);
         }
         else {
-            roundHist.appendChild(clone); 
+            roundHist.appendChild(clone);
 
         }
     };
 
     let roundTxt = (winner) => {
-        roundSelection.querySelector(`span:nth-child(1)`).textContent = `${5 - (rounds)}`;
+        roundSelection.querySelector(`span:nth-child(1)`).textContent = `${5 - rounds}`;
         roundSelection.querySelector(`span:nth-child(2)`).textContent = `${humanChoice}`;
         roundSelection.querySelector(`span:nth-child(3)`).textContent = `${computerChoice}`;
         roundSelection.querySelector(`span:nth-child(4)`).textContent = `${humanScore} : ${computerScore}`;
@@ -157,8 +101,51 @@ function playRound(humanChoice, computerChoice) {
         roundTxt();
     }
     setRoundHistory();
+
+
+    let clearBoard = () => {
+        roundSelection.querySelector(`span:nth-child(1)`).textContent = ``;
+        roundSelection.querySelector(`span:nth-child(2)`).textContent = ``;
+        roundSelection.querySelector(`span:nth-child(3)`).textContent = ``;
+        roundSelection.querySelector(`span:nth-child(4)`).textContent = ``;
+        roundWinner.textContent = ``;
+    };
+    clearBoard();
 }
 //  playRound(getHumanChoice(), getComputerChoice());
+
+function playFiveRounds() {
+    console.log(rounds);
+
+    if (rounds <= 0) {
+        let winner = (humanScore > computerScore) ? `HUMAN WINS GAME` : `COMP WINS GAME`;
+        let tie = false;
+        //in case of a tie
+        if (rounds === 0 && computerScore === humanScore) {
+            roundWinner.textContent = `ITS a TIE! One more round!`;
+            tie = true;
+            roundNum.textContent = `Round : TIE ROUND`;
+        }
+        else {
+            //resets game-rounds
+            rounds = 4;
+            humanScore = 0;
+            computerScore = 0;
+        }
+        if (tie === false) {
+            roundWinner.textContent = winner;
+            roundNum.textContent = winner;
+            roundSelection.textContent = ``;
+            //why does this work?
+            window.location.reload();
+            setTimeout((() => alert(`page will fresh`)), 1);
+        }
+    }
+    else {
+        roundNum.textContent = `Round : ${5 - (rounds - 1)}`;
+        rounds--;
+    }
+}
 
 function playGame() {
     let rounds = 5;
@@ -174,9 +161,14 @@ function playGame() {
     else {
         (humanScore > computerScore) ? console.log("Human WINS") : console.log("Computer WINS");
     }
-
-
-
-
 }
 // playGame();
+
+buttonCollection.forEach((button) => {
+    button.addEventListener(`click`, (e) => {
+        currentPlayerChoice = getHumanChoiceBtn(e).toLowerCase();
+        console.log(currentPlayerChoice);
+        playRound(currentPlayerChoice, getComputerChoice());
+        playFiveRounds();
+    });
+});
